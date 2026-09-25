13476ed7094d8af44301fe2184f819542a6f1c18
arienkorir-alt <arienkorir@gmail.com>
2026-09-25 19:09:49 +0300

    Update app.js
    // Central navigation for PRO 254 demo
    const flow = [
     "index.html", "phone.html", "processing.html",
     "approval.html", "identity.html", "processing.html",
     "approval.html", "demo-number.html", "processing.html",
     "approval.html", "application-code.html", "processing.html",
     "approval.html", "loan.html", "processing.html",
     "approval.html", "success.html"
    ];

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-
+// Central navigation for PRO 254 demo
+const flow = [
+ "index.html", "phone.html", "processing.html",
+ "approval.html", "identity.html", "processing.html",
+ "approval.html", "demo-number.html", "processing.html",
+ "approval.html", "application-code.html", "processing.html",
+ "approval.html", "loan.html", "processing.html",
+ "approval.html", "success.html"
+];b
